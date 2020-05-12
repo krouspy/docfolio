65af925b0b4c766be41498d4e5be1c8357e6aca0
krouspy <knjlau1@gmail.com>
2020-05-12 20:59:13 +0200

    add mongo

--- server/index.js
+++ server/index.js
@@ -1,17 +1,34 @@
-require("dotenv").config();
-const app = require("express")();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+require('dotenv').config();
+const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send({
-    statusCode: 200,
+app.get('/', (req, res) => {
+  MongoClient.connect(mongoURL, { useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      throw 'Error ' + err;
+    }
+    const collection = client.db('docfolio').collection('link');
+
+    collection.find({}).toArray((error, result) => {
+      if (error) {
+        throw 'Error ' + error;
+      }
+
+      client.close();
+      res.send({
+        statusCode: 200,
+        result: result,
+      });
+    });
   });
 });
 
