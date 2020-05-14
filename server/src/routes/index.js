c80e19fbed1abdb98ba7fa58ebbf2dec5a4b788c
krouspy <knjlau1@gmail.com>
2020-05-14 19:05:24 +0200

    change insert route endpoint

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/index');
 
 router.get('/all', controller.find_all);
-router.post('/insert', controller.insert_one);
+router.post('/add', controller.insert_one);
 
 module.exports = router;
