9cf3d7eca352d2441dd72d4897c539fe838029ae
krouspy <knjlau1@gmail.com>
2020-05-15 13:52:47 +0200

    fix categories + add start icons

--- client/src/components/AddForm/index.jsx
+++ client/src/components/AddForm/index.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import LinkIcon from '@material-ui/icons/Link';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
 
-export default ({ open, handleOpen }) => {
+export default ({ open, handleOpen, categories }) => {
   const [url, setURL] = useState('');
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState(categories[0]);
+
+  useEffect(() => {
+    setCategory(categories[0]);
+  }, [categories]);
 
   const add = () => {
     if (url && category) {
-      console.log(url);
       const postURL = 'http://localhost:3000/api/add';
       const options = {
         method: 'POST',
@@ -48,23 +55,43 @@ export default ({ open, handleOpen }) => {
       <DialogTitle id="form-dialog-title">Add Source</DialogTitle>
       <DialogContent dividers>
         <TextField
+          select
           autoFocus
+          value={category}
+          onChange={handleCategory}
+          label="category"
+          variant="outlined"
+          margin="dense"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <ViewModuleIcon />
+              </InputAdornment>
+            ),
+          }}
+        >
+          {categories.map(option => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
           value={url}
           onChange={handleURL}
           label="url"
           variant="outlined"
           type="text"
-          fullWidth
           margin="dense"
-        />
-        <TextField
-          value={category}
-          onChange={handleCategory}
-          label="category"
-          variant="outlined"
-          type="text"
           fullWidth
-          margin="dense"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LinkIcon />
+              </InputAdornment>
+            ),
+          }}
         />
       </DialogContent>
       <DialogActions>
