516868e9a437a3455a93077c9f4841e575adbdac
krouspy <knjlau1@gmail.com>
2020-06-02 00:30:17 +0200

    move server as root for repo + set start scripts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
-const assert = require('assert');
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path');
 const routes = require('./routes/index');
 
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use('/api', routes);
+
+app.get('/*', (_, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server listening in PORT ${PORT}`));
